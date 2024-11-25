3acf46c464352d6fba9bde25d5de334ca79e5485
IshuSahu <ishusahu593@gmail.com>
2024-11-25 10:22:21 +0530

    client changes for deploy

--- Ecommerce/client/src/store/auth-slice/index.js
+++ Ecommerce/client/src/store/auth-slice/index.js
@@ -97,7 +97,7 @@ const authSlice = createSlice({
   initialState,
   reducers: {
     setUser: (state, action) => {},
-    resetTokenAndCredentialss : (state) =>{
+    resetTokenAndCredentials : (state) =>{
       state.isAuthenticated = false,
       state.user = null,
       state.token = null
@@ -157,5 +157,5 @@ const authSlice = createSlice({
   },
 });
 
-export const {setUser,resetTokenAndCredentialss} = authSlice.actions;
+export const {setUser,resetTokenAndCredentials} = authSlice.actions;
 export default authSlice.reducer;
