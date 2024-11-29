a13460278509fa59dff58151e664b5d1166bb4f1
IshuSahu <ishusahu593@gmail.com>
2024-11-29 22:20:23 +0530

    Ddeployemnet related changes

--- Ecommerce/client/src/components/auth/AuthLayout.jsx
+++ Ecommerce/client/src/components/auth/AuthLayout.jsx
@@ -1,3 +1,4 @@
+import { HousePlug } from "lucide-react";
 import React from "react";
 import { Outlet } from "react-router-dom";
 
@@ -7,7 +8,7 @@ function AuthLayout() {
       <div className="hidden lg:flex items-center justify-center bg-black w-1/2 px-12">
         <div className=" max-w-md space-y-6 text-center text-primary-foreground">
           <h1 className=" text-4xl font-extrabold tracking-tight">
-            Welcome to Ecommerce Store
+            Welcome To ShopEase Store
           </h1>
         </div>
       </div>
